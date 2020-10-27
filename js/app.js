19ed89a1cecdcd5b26dd90059b88cdc8fd325db1
Nixieano <69241884+Nixieano@users.noreply.github.com>
2020-10-28 00:23:38 +0200

    Ofers and Slider

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".slider").slick({
+        slidesToShow:3,
+        variableWidth: true,
+        responsive:[
+            {
+                breakpoint: 1125,
+                settings: {
+                    slidesToShow:2
+                }
+            },{
+                breakpoint: 755,
+                settings: {
+                    slidesToShow:1
+                }
+            }
+        ]
+    });
+})
